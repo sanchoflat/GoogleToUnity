38da5620bb11c220a293d52caf7f2d5c61926318
Alexandr Eronin <sanchoflat@Gmail.com>
2016-07-11 09:49:52 +0300

    - остановил отслеживание ненужных meta

--- Assets/LoaderFromGame.cs
+++ Assets/LoaderFromGame.cs
@@ -1,34 +1,25 @@
-﻿using UnityEngine;
+﻿using G2U;
+using UnityEngine;
+
 public class LoaderFromGame : MonoBehaviour {
-    private const string GoogleSheetURL =
-        "http://docs.google.com/feeds/download/spreadsheets/Export?key={0}&exportFormat=csv&gid={1}";
-
-    // GUID таблицы
-    private readonly string _googleDriveFileGuid = "1Yu8V-W25wkAqHCAPXYR6wQXLzhClOMItYeXHScXXDCY";
-
-    // GUID листа в таблице
-    private readonly string _googleDriveSheetGuid = "0";
-
-    // Данные, полученные из гугл таблицы в CSV формате
-    private string _requestedData;
-
     private void Start() {
-        StartCoroutine(LoadSheet(GetURL()));
+//        var playerConfig = UnitConfig.LoadUnitConfig(Param.PlayerConfigPath);
+//        var archer = UnitConfig.LoadUnitConfig(Param.ArcherConfigPath);
+//        var arbalester = UnitConfig.LoadUnitConfig(Param.ArbalesterConfigPath);
+//        var spear = UnitConfig.LoadUnitConfig(Param.SpearConfigPath);
+//        DrawConfig(playerConfig);
+//        DrawConfig(archer);
+//        DrawConfig(arbalester);
+//        DrawConfig(spear);
     }
 
-    private System.Collections.IEnumerator LoadSheet(string URL)
-    {
-        var request = new WWW(URL);
-        yield return request;
-        _requestedData = request.text;
-        Debug.Log(_requestedData);
-
-
-        var data = CSVReader.Read(_requestedData);
-        Debug.Log(data);
-    }
-
-    private string GetURL() {
-        return string.Format(GoogleSheetURL, _googleDriveFileGuid, _googleDriveSheetGuid);
+    private void DrawConfig(UnitConfig c) {
+//       Debug.Log("---------------------");
+//        Debug.Log(c.Name + "  " + c.Name.GetType());
+//        Debug.Log(c.Speed + "  " + c.Speed.GetType());
+//        Debug.Log(c.Damage + "  " + c.Damage.GetType());
+//        Debug.Log(c.Health + "  " + c.Health.GetType());
+//        Debug.Log(c.IsMelee + "  " + c.IsMelee.GetType());
+//        Debug.Log(c.UpgradeCosts.Length + "  " + c.UpgradeCosts.GetType());
     }
 }
